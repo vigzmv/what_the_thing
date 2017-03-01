4e467614fbd3a47a3311db4b080ef7e4285672fb
vigzmv <vigzmv@outlook.com>
2017-03-01 15:31:12 +0530

    react-native-init

---
+++ android/app/src/main/java/com/what_the_thing/MainActivity.java
@@ -0,0 +1,15 @@
+package com.what_the_thing;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "what_the_thing";
+    }
+}
