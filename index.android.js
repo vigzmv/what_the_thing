9b540de8c32fae300316d685e2a53234c499cb34
vigzmv <vigzmv@outlook.com>
2017-03-01 19:32:58 +0530

    +rn-camera :camera:

--- index.android.js
+++ index.android.js
@@ -1,53 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	StyleSheet,
+	Dimensions,
+	Text,
+	View
 } from 'react-native';
+import Camera from 'react-native-camera';
 
 export default class what_the_thing extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Camera ref={(cam) => {
+                    this.camera = cam;
+                }}
+				style={styles.preview}
+				aspect={Camera.constants.Aspect.fill}>
+                    <Text style={styles.capture} onPress={()=>console.log(1)}>[CAPTURE]</Text>
+                </Camera>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  preview: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
 });
 
 AppRegistry.registerComponent('what_the_thing', () => what_the_thing);
