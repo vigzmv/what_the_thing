edba145fd58e5e053f706b2f7d6e50525e7daf03
vigzmv <vigzmv@outlook.com>
2017-03-02 22:25:33 +0530

    +react-native-vertor-icons

--- index.android.js
+++ index.android.js
@@ -4,20 +4,34 @@ import {
 	StyleSheet,
 	Dimensions,
 	Text,
-	View
+	View,
+    TouchableOpacity,
+    TouchableHighlight
 } from 'react-native';
 import Camera from 'react-native-camera';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 export default class what_the_thing extends Component {
-    render() {
+	render() {
         return (
             <View style={styles.container}>
                 <Camera ref={(cam) => {
                     this.camera = cam;
                 }}
+
 				style={styles.preview}
-				aspect={Camera.constants.Aspect.fill}>
-                    <Text style={styles.capture} onPress={()=>console.log(1)}>[CAPTURE]</Text>
+				aspect={Camera.constants.Aspect.fill}
+				type={Camera.constants.Type.back}
+                playSoundOnCapture={true}
+                captureMode={Camera.constants.CaptureMode.still}
+                captureQuality={Camera.constants.CaptureQuality.high}
+                >
+                    <TouchableOpacity onPress={()=>alert(1)}>
+                        <View style={styles.cameraIco}>
+                            <Icon name="camera" size={50} color="#E8EAF6" />
+                        </View>
+                    </TouchableOpacity>
                 </Camera>
             </View>
         );
@@ -25,24 +39,19 @@ export default class what_the_thing extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    height: Dimensions.get('window').height,
-    width: Dimensions.get('window').width
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    color: '#000',
-    padding: 10,
-    margin: 40
-  }
+    container: {
+        flex: 1
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width
+    },
+    cameraIco:{
+        marginBottom: 50
+    }
 });
 
 AppRegistry.registerComponent('what_the_thing', () => what_the_thing);
