c5686d05fe1ddfa24a1518f9d619b14295daaa5b
vigzmv <vigzmv@outlook.com>
2017-03-03 16:07:51 +0530

    +es2015 for clarifai, Concept recog

--- index.android.js
+++ index.android.js
@@ -1,53 +1,65 @@
 import React, {Component} from 'react';
 import {
-	AppRegistry,
-	StyleSheet,
-	Dimensions,
-	Text,
-	View,
+    AppRegistry,
+    StyleSheet,
+    Dimensions,
+    Text,
+    View,
     TouchableOpacity,
     TouchableHighlight
 } from 'react-native';
+import Camera from 'react-native-camera';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-var Clarifai = require('clarifai');    
+var Clarifai = require('clarifai');
 var app = new Clarifai.App(
     'NC_zCTITbajnUlNUJ8LrFMl4FH5tl-1Bl8nSp30p',
     '1QoTBYaR8MDQRIr_rRHI3RDVvYeMyslkHtq7D9BY'
 );
 
-import Camera from 'react-native-camera';
-import Icon from 'react-native-vector-icons/FontAwesome';
-
 export default class what_the_thing extends Component {
 
     takePicture() {
         this.camera.capture()
-          .then((image64) => {
-
-          })
-          .catch(err => console.error(err));
+            .then((image64) => {
+                console.log("cap");
+                app.models.predict(Clarifai.GENERAL_MODEL, {base64: image64.data})
+                    .then(function(response) {
+                        const concepts = (response.outputs[0].data.concepts.slice(0,3)).map(
+                            concept => ({name:concept.name, val: concept.value})
+                        );
+                        console.table(concepts);
+                        alert(concepts);
+                        
+                    }, function(err) {
+                        alert(err);
+                    });
+            })
+            .catch(err => alert(err));
     }
 
-
-	render() {
+    render() {
         return (
             <View style={styles.container}>
                 <Camera ref={(cam) => {
                     this.camera = cam;
                 }}
 
-				style={styles.preview}
-				aspect={Camera.constants.Aspect.fill}
-				type={Camera.constants.Type.back}
+                style={styles.preview}
+                aspect={Camera.constants.Aspect.fill}
+                type={Camera.constants.Type.back}
                 playSoundOnCapture={true}
                 captureMode={Camera.constants.CaptureMode.still}
+                captureTarget={Camera.constants.CaptureTarget.memory}
                 captureQuality={Camera.constants.CaptureQuality.high}
                 >
+
                     <TouchableOpacity onPress={this.takePicture.bind(this)}>
                         <View style={styles.cameraIco}>
-                            <Icon name="camera" size={50} color="#E8EAF6" />
+                            <Icon name="camera" size={50} color="#E8EAF6"/>
                         </View>
                     </TouchableOpacity>
+
                 </Camera>
             </View>
         );
@@ -65,7 +77,7 @@ const styles = StyleSheet.create({
         height: Dimensions.get('window').height,
         width: Dimensions.get('window').width
     },
-    cameraIco:{
+    cameraIco: {
         marginBottom: 50
     }
 });
